df0ea742866f9af90794d424631dbed3c38446d1
sergei lauffenburger <lauffenburger@live.fr>
2022-04-14 18:09:41 +0200

    init boucle serie

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SeriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SerieController extends AbstractController
 {
     #[Route('/serie', name: 'app_serie')]
-    public function index(): Response
+    public function index(SeriesRepository $series): Response
     {
         return $this->render('serie/serie.html.twig', [
             'controller_name' => 'SerieController',
             'page_title'=>'Series',
+            'series'=>$series->findAll(),
          
         ]);
     }
