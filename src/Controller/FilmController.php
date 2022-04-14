4d7fcea0e2082e1991cbe4def2b46d1d5d48a1fa
Marie Agniel <agniel.marie@gmail.com>
2022-04-14 17:05:35 +0200

    init boucle film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,18 +2,74 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Films;
+use App\Repository\FilmsRepository;
 
 class FilmController extends AbstractController
 {
     #[Route('/film', name: 'app_film')]
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine,FilmsRepository $films): Response
+    {        
+        //Récupere l'object en fonction de l'id 
         return $this->render('film/film.html.twig', [
             'controller_name' => 'FilmController',
-            'page_title' => 'Films'
+            'page_title' => 'Films',
+            'films'=>$films->findAll()
         ]);
     }
+
+    /**
+     * Affiche les détails d'un film
+     */
+    #[Route('/film/{$id}', name: 'film.show')]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $film = $doctrine->getRepository(Films::class)->find($id);
+        if(!$film){
+            throw $this->createNotFoundException("Aucun film ne correspond à l'identifiant ".$id);
+        };
+        return new Response('Aller voir ce super film appelé '.$film->getTitre());
+    }
+
+
+    // /**
+    //  * Permet l'ajout de film dans la base de donnée
+    //  *
+    //  * @param ManagerRegistry $doctrine
+    //  * @return Response
+    //  */
+    // #[Route('/film/ajout',name: 'film.ajout')]
+    // public function ajoutFilm(Request $request, ManagerRegistry $doctrine): Response
+    // {
+    //     $entityManager = $doctrine->getManager();
+
+    //     $film = new Films();
+    //     $film->setTitre("Dune")
+    //         ->setAnnee(2021)
+    //         ->setGenre1(4)
+    //         ->setGenre2(3)
+    //         ->setGenre3(1)
+    //         ->setActeurs([
+    //             "Timothée Chalamet",
+    //             "Rebecca Ferguson",
+    //             "Oscar Isaac"
+    //         ])
+    //         ->setSynopsis("L'histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du commun qui le dépasse totalement. Car s'il veut préserver l'avenir de sa famille et de son peuple, il devra se rendre sur la planète la plus dangereuse de l'univers – la seule à même de fournir la ressource la plus précieuse au monde, capable de décupler la puissance de l'humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à dominer leur peur pourront survivre…")
+    //         ->setImages("assets/website/images/films/4633954.jpg")
+    //         ->setDuree(156);
+    //         // dit a Doctrine que  tu veux (éventuellement) sauvegarder ce film (la requête n'est pas encore envoyer)
+    //         $entityManager->persist($film);
+
+    //         // Envoie, à ce moment là, la requête d'ajout dans la base de donnée (INSERT)
+    //         $entityManager->flush();
+
+    //         return new Response("Nouveau film enregistré avec comme id ".$film->getId());
+
+    // }
+
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ Encore
   // directory where compiled assets will be stored
   // .setOutputPath("public/build/")
   .setOutputPath("public/build/website/")
+  .copyFiles({
+        from: './assets/images',
+
+         // optional target path, relative to the output dir
+         to: 'images/[path][name].[ext]',
+
+         // if versioning is enabled, add the file hash too
+         //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+         //pattern: /\.(png|jpg|jpeg)$/
+     })
   // public path used by the web server to access the output path
   // n.setPublicPath("/build")
   .setPublicPath("/build/website")
